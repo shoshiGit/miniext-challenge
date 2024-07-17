8abb87a21e20a213e37204b20da170ec6442c5b8
shoshiGit <shoshiwolpe@gmail.com>
2024-07-17 21:31:27 +0300

    first commit

---
+++ components/firebase/firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
+import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
+// import { isDev } from '../isDev';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDy_mOL8dw9BjzfDzO46HNUbORKC-ByY7I',
+    authDomain: 'miniextentions-challenge.firebaseapp.com',
+    projectId: 'miniextentions-challenge',
+    storageBucket: 'miniextentions-challenge.appspot.com',
+    messagingSenderId: '262072501499',
+    appId: '1:262072501499:web:37974e9a694d36a1869c5e'
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(firebaseApp);
+export const baseBucketName = 'miniextentions-challenge';
+
+/* if (isDev) {
+    connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
+} */
